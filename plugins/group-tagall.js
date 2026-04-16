8e74511549e4f125a496e015a51382dd610950ec
bilalnadeem3149-sketch <bilalnadeem3149@gmail.com>
2026-04-17 00:24:08 +0500

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -44,7 +44,7 @@ async (conn, mek, m, { from, participants, reply, isGroup, senderNumber, groupAd
             teks += `${randomEmoji} @${mem.id.split('@')[0]}\n`;
         }
 
-        teks += "└──✪ INCONNU XD ✪──";
+        teks += "└──✪ BILAL XD ✪──";
 
         conn.sendMessage(from, { text: teks, mentions: participants.map(a => a.id) }, { quoted: mek });
 
@@ -54,4 +54,4 @@ async (conn, mek, m, { from, participants, reply, isGroup, senderNumber, groupAd
     }
 });
 
-      
+
